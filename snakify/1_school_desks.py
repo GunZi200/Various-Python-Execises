d78daeade9aba2936202016d0f428bd8a4304ef2
gmg19 <gmg19@hi.is>
2019-05-11 10:22:36 +0000

    11/05/2019

---
+++ snakify/1_school_desks.py
@@ -0,0 +1,34 @@
+# http://www.snakify.org/en/lessons/print_input_numbers/problems/school_desks/
+# A school decided to replace the desks in three classrooms. 
+# Each desk sits two students. Given the number of students in each class, 
+# print the smallest possible number of desks that can be purchased.
+
+#The program should read three integers: the number of students in each of the three classes, 
+# a, b and c respectively.
+
+#In the first test there are three groups. The first group has 20 students and 
+# thus needs 10 desks. The second group has 21 students, so they can get by 
+# with no fewer than 11 desks. 11 desks is also enough for the third group of 22 
+# students. So we need 32 desks in total.
+
+# Read an integer:
+def readInt(): return int(input())
+
+myList = []
+
+# Get user input
+myList.append(readInt())
+myList.append(readInt())
+myList.append(readInt())
+
+desksTotal = 0
+
+# Calculate desks needed
+for x in range(3): 
+    if myList[x] % 2 == 0: desksTotal += myList[x] // 2
+    else: desksTotal += myList[x] // 2 + 1
+
+# Answer
+print(desksTotal)
+
+
